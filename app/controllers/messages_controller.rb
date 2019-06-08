444551c0bce9ba20f45f1a006374910107cb902b
Saki-Ko <mi.youri@gmail.com>
2019-06-08 17:50:48 +0900

    Update message_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
   def index
+    @messages = [{user: "seo",text: "Hello world!",time: "2019/01/01 00:00"},{user: "neko",text: "Good bye world!",time: "2019/01/02 00:00"}]
   end
 end
