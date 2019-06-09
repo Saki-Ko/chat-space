d010630ac5aacb29fb09fd2f476297ac9c0a3ff2
Saki-Ko <mi.youri@gmail.com>
2019-06-09 17:32:57 +0900

    Add_ImageMagic

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,8 @@ class MessagesController < ApplicationController
   def index
     @messages = [{user: "seo",text: "Hello world!",time: "2019/01/01 00:00"},{user: "neko",text: "Good bye world!",time: "2019/01/02 00:00"}]
   end
+  
+  def creage
+    @message = "メッセージ"
+  end
 end
